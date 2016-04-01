69e7289f64afff3de4742bf53fe45d816839b63b
Brandon Espinoza <espinozab100@gmail.com>
2016-03-31 22:00:16 -0500

    Navbar's links have hover effect

--- sections/nav.php
+++ sections/nav.php
@@ -14,18 +14,15 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <li class="hvr-sweep-to-bottom"><a href="#">Home</a></li>
+            <li class="hvr-sweep-to-bottom"><a href="#">About Us</a></li>
+            <li class="dropdown hvr-sweep-to-bottom">
+              <a href="#" class=" dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">Separated link</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="#">One more separated link</a></li>
+                <li><a class="hvr-sweep-to-right" href="#">Spring</a></li>
+                <li><a class="hvr-sweep-to-right" href="#">Summer</a></li>
+                <li><a class="hvr-sweep-to-right" href="#">Fall</a></li>
+                <li><a class="hvr-sweep-to-right" href="#">Winter</a></li>
               </ul>
             </li>
           </ul>
