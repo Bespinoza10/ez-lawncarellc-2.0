658ddc23c301e22b3a0fd13d816e51f9beb0f138
Brandon Espinoza <espinozab100@gmail.com>
2016-04-04 11:50:04 -0500

    Created Seasonal Pages with services offered in those seasons and a quick tip (WIP)

---
+++ spring-services.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+		  $pagetitle = "EZ-Lawncare | Spring Services";
+		  include('sections/head.php')
+		?>
+  </head>
+  <body>
+
+    <?php include('sections/nav.php')?>
+
+
+    <div class=" container-fluid spring-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="center-text spring-titles" style="font-size: 60px;">Spring Services</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 paddingTop-1">
+          <h1 class="srvc-title">Why should I start a seasonal maintenance in Spring? </h1>
+          <br>
+          <p style="font-style: italic;"> COMING SOON </p>
+        </div>
+        <div class="col-md-5 col-md-offset-1 paddingTop-1">
+          <h1 class="srvc-title">Our Spring Services</h1>
+          <hr>
+          <div class="col-md-6">
+            <p class="srvc-p">Lawn Mowing</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Edging</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Blow</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Pre-Emergent & Fertilizing</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Aeration, Overseeding & Starter Fertilizer</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Planting Season Flowers, Shrubs & Trees</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Mulch</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Trimming</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-p">Storm Clean Up</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+    <?php include('sections/scripts.php')?>
+  </body>
+</html>
---
+++ winter-services.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+		  $pagetitle = "EZ-Lawncare | Winter Services";
+		  include('sections/head.php')
+		?>
+  </head>
+  <body>
+
+    <?php include('sections/nav.php')?>
+
+
+    <div class=" container-fluid winter-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="center-text spring-titles" style="font-size: 60px;">Winter Services</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 paddingTop-1">
+          <h1 class="srvc-title">What to do to protect your plants and shrubs in the winter?</h1>
+          <br>
+          <p style="font-style: italic;">COMING SOON</p>
+        </div>
+        <div class="col-md-5 col-md-offset-1 paddingTop-1">
+          <h1 class="srvc-title">Our Winter Services</h1>
+          <hr>
+          <div class="col-md-6">
+            <p class="srvc-p">Snow Removal*</p>
+          </div>
+
+          <div class="col-md-6">
+            <p class="srvc-winter-p">* All of our services are available all year long. </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+    <?php include('sections/scripts.php')?>
+  </body>
+</html>
